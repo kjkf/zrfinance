fe1c95c83339754617338c586750d2b41701f654
kjkf <yakjkf@gmail.com>
2024-02-05 13:54:59 +0600

    request from 2 databases

---
+++ app/Controllers/Dbs.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\MaterialsModel;
+
+class Dbs extends BaseController
+{
+    public function index()
+    {
+       $db1 = db_connect();
+       $model = new MaterialsModel($db1);
+
+       $db2 = db_connect("db_class");
+       $model2 = new MaterialsModel($db2);
+
+       echo '<pre>';
+       print_r($model->getAllUnits());
+       echo '<pre><hr>';
+
+       echo '<pre>';
+       print_r($model2->getFromAnotherDB());
+       echo '<pre><hr>';
+    }
+}
